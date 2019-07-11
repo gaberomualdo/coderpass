a1a030e41bf8f10e36d90d0b6b704c00d3cc59bc
xtrp <36894883+xtrp@users.noreply.github.com>
2019-07-11 14:14:11 +0100

    added font files and updated main.js with app events such as quitting when all windows are closed

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 
+// global object window
+let window;
+
 function createWindow () {
   
-  // Create window.
-  let window = new BrowserWindow({
+  // create window
+  window = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -17,3 +20,18 @@ function createWindow () {
 
 // create window
 app.on('ready', createWindow);
+
+// quit when all windows are closed
+app.on('window-all-closed', function () {
+    // on macOS apps stay open until quit in menu-bar or w/Cmd+Q
+    if (process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    // on macOS re-create app window when app icon is clicked
+    if (window === null){
+        createWindow();
+    }
+});
