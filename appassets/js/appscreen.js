76e48b38a9fb2dc2e98d10f05b846e5cee370b75
xtrp <36894883+xtrp@users.noreply.github.com>
2019-11-19 17:27:28 +0000

    add copy btn functionality, will add CSS and tooltip soon

--- appassets/js/appscreen.js
+++ appassets/js/appscreen.js
@@ -8,9 +8,10 @@ const getHTMLOfAccountBlock = (accountID, justDisplayHTML) => {
 
     // create properties HTML
     let propertiesHTML = "";
+    let copyClipboardSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21 2h-19v19h-2v-21h21v2zm3 2v20h-20v-20h20zm-2 2h-1.93c-.669 0-1.293.334-1.664.891l-1.406 2.109h-6l-1.406-2.109c-.371-.557-.995-.891-1.664-.891h-1.93v16h16v-16zm-3 6h-10v1h10v-1zm0 3h-10v1h10v-1zm0 3h-10v1h10v-1z"/></svg>';
     Object.keys(accountObj.properties).forEach((propertyName) => {
         const propertyValue = accountObj.properties[propertyName];
-        propertiesHTML += "<li><p class='name'>" + propertyName + ":</p><p class='value default_style_code'>" + propertyValue + "</p></li>";
+        propertiesHTML += "<li><p class='name'>" + propertyName + ":</p><p class='value default_style_code'>" + propertyValue + "</p><button class='copy_property_btn' onclick='eventCopyPropertyValueBtn(this);'>" + copyClipboardSVG + "</button></li>";
     });
 
     // display HTML
@@ -209,6 +210,11 @@ const eventDownloadPasswordDatafileBtn = () => {
 
     // download password datafile
     downloadURI("vault/data.txt", "password_datafile.txt");
+}
+
+// copy property btn
+const eventCopyPropertyValueBtn = (btn) => {
+    navigator.clipboard.writeText(btn.previousSibling.innerText);
 }
 
 // map add account button onclick to event function
