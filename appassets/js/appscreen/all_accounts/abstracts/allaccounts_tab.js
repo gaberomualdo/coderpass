22e0077bd66c7c846bd48edd0cfc4c1f6af37eaa
xtrp <36894883+xtrp@users.noreply.github.com>
2020-02-26 18:59:12 +0000

    get everything back up and running with new design and HTML structure

--- appassets/js/appscreen/all_accounts/abstracts/allaccounts_tab.js
+++ appassets/js/appscreen/all_accounts/abstracts/allaccounts_tab.js
@@ -2,7 +2,7 @@ let vaultContents;
 let vaultPassword;
 
 // function when all accounts tab is opened
-const allAccounts__openTab = (passedVaultPassword) => {
+const allAccounts__openTab = () => {
     // loop through each account and display
     Object.keys(vaultContents.accounts).forEach((accountID) => {
         // add current account block
