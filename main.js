d5dc2d397e7741ca07f4cef3a4a8dd18fb01ad94
xtrp <36894883+xtrp@users.noreply.github.com>
2020-07-08 14:44:07 -0400

    Write basic exit vault styles

--- main.js
+++ main.js
@@ -5,13 +5,13 @@
 */
 
 // set constant global variables for application
-global.APP_VERSION = "0.1.0";
+global.APP_VERSION = '0.1.0';
 
 // get required modules from electron
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 // get Node.js modules used in app
-const fs = require("fs");
+const fs = require('fs');
 
 // global window object
 let window;
@@ -26,46 +26,47 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
     },
+    allowRunningInsecureContent: true,
   };
 
-  if (process.platform === "darwin") {
-    windowOptions.titleBarStyle = "hiddenInset";
+  if (process.platform === 'darwin') {
+    windowOptions.titleBarStyle = 'hiddenInset';
   }
 
   // create window with options
   window = new BrowserWindow(windowOptions);
 
   // load main HTML file into window with platform info
-  window.loadFile("index.html");
+  window.loadFile('index.html');
 
   // set variable for onblur and onfocus
   let windowIsFocused = false;
-  window.on("focus", () => {
+  window.on('focus', () => {
     windowIsFocused = true;
   });
-  window.on("blur", () => {
+  window.on('blur', () => {
     windowIsFocused = false;
   });
 
   // send focused info as reply to ipc message from renderer process
-  ipcMain.on("is-window-focused", (event) => {
+  ipcMain.on('is-window-focused', (event) => {
     event.returnValue = windowIsFocused;
   });
 }
 
 // create window
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
 // quit when all windows are closed
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // on macOS apps stay open until quit in menu-bar or w/Cmd+Q
-  if (process.platform !== "darwin") {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 // on macOS re-create app window when app icon is clicked
-app.on("activate", () => {
+app.on('activate', () => {
   if (window === null) {
     createWindow();
   }
