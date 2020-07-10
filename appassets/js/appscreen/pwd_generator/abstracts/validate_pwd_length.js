a514629d234b66ea2b2fb462f6bd5c903c722d51
xtrp <36894883+xtrp@users.noreply.github.com>
2020-07-09 22:04:11 -0400

    finish password length validation

---
+++ appassets/js/appscreen/pwd_generator/abstracts/validate_pwd_length.js
@@ -0,0 +1,22 @@
+const validatePwdLength = (input) => {
+  const min = 0;
+  const max = 9999;
+
+  const rawVal = input.value.toString();
+
+  if (rawVal.includes('e')) {
+    return (input.value = min);
+  }
+  if (rawVal.length <= 0) {
+    return (input.value = min);
+  }
+
+  const val = parseInt(rawVal);
+
+  if (val < min) {
+    return (input.value = min);
+  }
+  if (val > max) {
+    return (input.value = max);
+  }
+};
