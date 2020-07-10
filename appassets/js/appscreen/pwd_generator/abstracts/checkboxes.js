3fc7ba2ba5e22dceb91391b6fc4c294ff1b197e0
xtrp <36894883+xtrp@users.noreply.github.com>
2020-07-09 21:21:59 -0400

    fix issues with checkboxes

--- appassets/js/appscreen/pwd_generator/abstracts/checkboxes.js
+++ appassets/js/appscreen/pwd_generator/abstracts/checkboxes.js
@@ -1,9 +1,11 @@
 (() => {
-  const checkboxes = document.querySelectorAll('div.tab.pwd_generator .main .checkboxes li div.checkbox');
+  const checkboxes = Array.from(document.querySelectorAll('div.tab.pwd_generator .main .checkboxes li div.checkbox'));
+  const checkboxLabels = Array.from(document.querySelectorAll('div.tab.pwd_generator .main .checkboxes li div.checkbox + span'));
 
-  checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('click', () => {
-      checkbox.classList.toggle('checked');
+  checkboxes.concat(checkboxLabels).forEach((btn) => {
+    btn.addEventListener('click', () => {
+      console.log('checkbox btn clicked');
+      btn.parentElement.querySelector('.checkbox').classList.toggle('checked');
     });
   });
 })();
