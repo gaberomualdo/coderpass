a0a937bfc299f4c93492ca83bb4ac0880ce6dffa
xtrp <36894883+xtrp@users.noreply.github.com>
2020-07-17 00:31:33 -0400

    Fix bug with adding an account while others are being edited

--- appassets/js/appscreen/all_accounts/nav_components/add_account_btn.js
+++ appassets/js/appscreen/all_accounts/nav_components/add_account_btn.js
@@ -2,9 +2,22 @@
 const eventAddAccountBtn = () => {
   addAccountBtnElement.blur();
 
+  // only allow adding accounts if nothing is being edited
+  let amountOfAccountsBeingEdited = 0;
+  Array.from(document.querySelectorAll('div.tab.all_accounts > ul.accounts > div.account')).forEach((accountEl) => {
+    if (accountEl.classList.contains('editing')) {
+      amountOfAccountsBeingEdited++;
+    }
+  });
+
   // scroll to top
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+
+  if (amountOfAccountsBeingEdited > 0) {
+    alert(`Cannot Add New Account: Finish Editing ${amountOfAccountsBeingEdited === 1 ? 'An Account' : 'Accounts'} First`);
+    return;
+  }
 
   // when button is clicked, add semi-empty account
   const newAccountID = addAccountToDatabase({
