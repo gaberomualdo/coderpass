9c98b59e8272f16fca827f3cc17b9a429ab17fb7
xtrp <36894883+xtrp@users.noreply.github.com>
2020-07-29 20:36:17 -0400

    Fix simultaneous editing issue

--- appassets/js/appscreen/all_accounts/account_block_abstracts/code_editors.js
+++ appassets/js/appscreen/all_accounts/account_block_abstracts/code_editors.js
@@ -5,139 +5,131 @@ const codeEditors = {};
 const refreshCodeEditor = (accountID, value) => {
   // set value of editor to given value
   const editorElement = document.getElementById('codeeditor_' + accountID);
+  editorElement.innerHTML = '';
 
-  if (editorElement.innerHTML.trim().length > 0) {
-    // editor has already been created, so just set the current one's value
-    monacoSandbox(function (amdRequire) {
-      amdRequire(['vs/editor/editor.main'], () => {
-        codeEditors[accountID].setValue(value);
+  monacoSandbox(function (amdRequire) {
+    amdRequire(['vs/editor/editor.main'], () => {
+      // editor not created yet, so create one in codeeditor container
+      codeEditors[accountID] = monaco.editor.create(editorElement, {
+        value,
+        scrollBeyondLastLine: false,
+        theme: 'vs-dark',
+        lineNumbers: 'off',
+        roundedSelection: true,
+        fontSize: '14px',
+        automaticLayout: true,
+        dragAndDrop: true,
+        formatOnType: true,
+        copyWithSyntaxHighlighting: false,
+
+        language: 'json',
+
+        scrollbar: {
+          verticalScrollbarSize: 8,
+          horizontalScrollbarSize: 8,
+        },
+
+        // no minimap
+        minimap: {
+          enabled: false,
+        },
+
+        // no context menu
+        contextmenu: false,
+
+        // no overview ruler
+        hideCursorInOverviewRuler: true,
+        overviewRulerBorder: false,
+        overviewRulerLanes: 0,
       });
-    });
-  } else {
-    monacoSandbox(function (amdRequire) {
-      amdRequire(['vs/editor/editor.main'], () => {
-        // editor not created yet, so create one in codeeditor container
-        codeEditors[accountID] = monaco.editor.create(document.getElementById('codeeditor_' + accountID), {
-          value,
-          scrollBeyondLastLine: false,
-          theme: 'vs-dark',
-          lineNumbers: 'off',
-          roundedSelection: true,
-          fontSize: '14px',
-          automaticLayout: true,
-          dragAndDrop: true,
-          formatOnType: true,
-          copyWithSyntaxHighlighting: false,
 
-          language: 'json',
+      codeEditors[accountID]._standaloneKeybindingService.addDynamicKeybinding('-editor.action.nextMatchFindAction');
 
-          scrollbar: {
-            verticalScrollbarSize: 8,
-            horizontalScrollbarSize: 8,
-          },
+      // loaded set add loaded attributeto editor
+      document.getElementById('codeeditor_' + accountID).classList.add('loaded');
 
-          // no minimap
-          minimap: {
-            enabled: false,
-          },
+      // add/remove focused attribute of editor container when editor is focused or blurred accordingly
+      codeEditors[accountID].onDidFocusEditorWidget(() => {
+        document
+          .getElementById('accountid_' + accountID)
+          .querySelector('section.edit div.editor_container')
+          .classList.add('focused');
+      });
+      codeEditors[accountID].onDidBlurEditorWidget(() => {
+        document
+          .getElementById('accountid_' + accountID)
+          .querySelector('section.edit div.editor_container')
+          .classList.remove('focused');
+      });
 
-          // no context menu
-          contextmenu: false,
+      // on change, check for any errors in JSON and show them as tooltip in done editing icon
+      codeEditors[accountID].onDidChangeModelContent(() => {
+        const editorContent = codeEditors[accountID].getValue();
 
-          // no overview ruler
-          hideCursorInOverviewRuler: true,
-          overviewRulerBorder: false,
-          overviewRulerLanes: 0,
-        });
+        const errors = [];
 
-        codeEditors[accountID]._standaloneKeybindingService.addDynamicKeybinding('-editor.action.nextMatchFindAction');
+        if (!isValidJSONString(editorContent)) {
+          errors.push('Invalid JSON');
+        } else {
+          const parsedEditorContent = JSON.parse(editorContent);
+          if (
+            parsedEditorContent.name != undefined &&
+            parsedEditorContent.properties != undefined &&
+            typeof parsedEditorContent.name == 'string' &&
+            typeof parsedEditorContent.properties == 'object'
+          ) {
+            // save new value in vault
+            vaultContents.accounts[accountID] = parsedEditorContent;
 
-        // loaded set add loaded attributeto editor
-        document.getElementById('codeeditor_' + accountID).classList.add('loaded');
+            // refresh database
+            refreshVaultDatabase();
 
-        // add/remove focused attribute of editor container when editor is focused or blurred accordingly
-        codeEditors[accountID].onDidFocusEditorWidget(() => {
-          document
-            .getElementById('accountid_' + accountID)
-            .querySelector('section.edit div.editor_container')
-            .classList.add('focused');
-        });
-        codeEditors[accountID].onDidBlurEditorWidget(() => {
-          document
-            .getElementById('accountid_' + accountID)
-            .querySelector('section.edit div.editor_container')
-            .classList.remove('focused');
-        });
-
-        // on change, check for any errors in JSON and show them as tooltip in done editing icon
-        codeEditors[accountID].onDidChangeModelContent(() => {
-          const editorContent = codeEditors[accountID].getValue();
-
-          const errors = [];
-
-          if (!isValidJSONString(editorContent)) {
-            errors.push('Invalid JSON');
+            // refresh account block
+            refreshAccountBlock(accountID);
           } else {
-            const parsedEditorContent = JSON.parse(editorContent);
-            if (
-              parsedEditorContent.name != undefined &&
-              parsedEditorContent.properties != undefined &&
-              typeof parsedEditorContent.name == 'string' &&
-              typeof parsedEditorContent.properties == 'object'
-            ) {
-              // save new value in vault
-              vaultContents.accounts[accountID] = parsedEditorContent;
-
-              // refresh database
-              refreshVaultDatabase();
-
-              // refresh account block
-              refreshAccountBlock(accountID);
-            } else {
-              // throw error, and revert back to editing
-              if (parsedEditorContent.name == undefined) {
-                errors.push("Missing: 'name'");
-              } else if (parsedEditorContent.properties == undefined) {
-                errors.push("Missing: 'properties'");
-              } else if (!(typeof parsedEditorContent.name == 'string')) {
-                errors.push("'name' must be a String");
-              } else if (!(typeof parsedEditorContent.properties == 'object')) {
-                errors.push("'properties' must an Object");
-              }
+            // throw error, and revert back to editing
+            if (parsedEditorContent.name == undefined) {
+              errors.push("Missing: 'name'");
+            } else if (parsedEditorContent.properties == undefined) {
+              errors.push("Missing: 'properties'");
+            } else if (!(typeof parsedEditorContent.name == 'string')) {
+              errors.push("'name' must be a String");
+            } else if (!(typeof parsedEditorContent.properties == 'object')) {
+              errors.push("'properties' must an Object");
             }
           }
+        }
 
-          if (errors.length > 0) {
-            document.getElementById('accountid_' + accountID).classList.add('editing_error');
-            document
-              .getElementById('accountid_' + accountID)
-              .querySelector('div.buttons > button.edit')
-              .setAttribute('aria-label', errors.join('\n'));
-            document
-              .getElementById('accountid_' + accountID)
-              .querySelector('div.buttons > button.edit')
-              .setAttribute('data-balloon-pos', 'left');
-            document
-              .getElementById('accountid_' + accountID)
-              .querySelector('div.buttons > button.edit')
-              .setAttribute('disabled', 'true');
-          } else {
-            document.getElementById('accountid_' + accountID).classList.remove('editing_error');
-            document
-              .getElementById('accountid_' + accountID)
-              .querySelector('div.buttons > button.edit')
-              .removeAttribute('aria-label');
-            document
-              .getElementById('accountid_' + accountID)
-              .querySelector('div.buttons > button.edit')
-              .removeAttribute('data-balloon-pos');
-            document
-              .getElementById('accountid_' + accountID)
-              .querySelector('div.buttons > button.edit')
-              .removeAttribute('disabled');
-          }
-        });
+        if (errors.length > 0) {
+          document.getElementById('accountid_' + accountID).classList.add('editing_error');
+          document
+            .getElementById('accountid_' + accountID)
+            .querySelector('div.buttons > button.edit')
+            .setAttribute('aria-label', errors.join('\n'));
+          document
+            .getElementById('accountid_' + accountID)
+            .querySelector('div.buttons > button.edit')
+            .setAttribute('data-balloon-pos', 'left');
+          document
+            .getElementById('accountid_' + accountID)
+            .querySelector('div.buttons > button.edit')
+            .setAttribute('disabled', 'true');
+        } else {
+          document.getElementById('accountid_' + accountID).classList.remove('editing_error');
+          document
+            .getElementById('accountid_' + accountID)
+            .querySelector('div.buttons > button.edit')
+            .removeAttribute('aria-label');
+          document
+            .getElementById('accountid_' + accountID)
+            .querySelector('div.buttons > button.edit')
+            .removeAttribute('data-balloon-pos');
+          document
+            .getElementById('accountid_' + accountID)
+            .querySelector('div.buttons > button.edit')
+            .removeAttribute('disabled');
+        }
       });
     });
-  }
+  });
 };
